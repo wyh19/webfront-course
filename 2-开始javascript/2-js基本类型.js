f82475b6e00b3b6efd3d6c8cd2d7e1317ae0c564
王焱辉 <wyh_19@163.com>
2018-12-12 16:48:49 +0800

    开发

--- 2-开始javascript/2-js基本类型.js
+++ 2-开始javascript/2-js基本类型.js
@@ -1,8 +1,17 @@
+/**
+ * 任何编程语言都有变量，而变量又分为基本类型和引用类型（或者说对象类型）
+ * 基本类型：就是用于存放最简单值类型的变量类型，
+ * 比如数字、一串文字（官话：字符串）、是否（官话：布尔型）、空（js里有两种空，一种是null,一种是undefined,初学者认为这两者是一样即可）
+ * 基本类型的特点是变量直接访问内存里的值，变量a赋值给变量b时，b得到了一份全新的值，改变a的值，不会影响到b
+ * js中，定义变量的形式是 ： var 变量名
+ * 定义时一般会赋值，则形式为： var 变量名 = 值
+ */
+
 //数值
 var num_demo = 1
 
 //字符串
-var str_demo1 = "1"
+var str_demo1 = "1"  //打印面板看到的是1，没有双引号，因为双引号是代码的一部分，而不是值的一部分
 var str_demo2 = "Hello World"
 
 //布尔
@@ -35,3 +44,12 @@ console.log(typeof(bool_demo1))
 console.log(typeof(bool_demo2))
 console.log(typeof(null_demo)) //这个应该是null类型才对，但是因为历史遗留问题，js语法已经无法挽回这个错误了
 console.log(typeof(undefined_demo))
+
+//变量a赋值给变量b
+var a = 1
+var b = a
+//修改a的值
+a = 2
+//查看a和b的值,可以印证，a的值和b的值都是各自独立的，不会相互影响
+console.log('a当前值为'+a)
+console.log('b当前值为'+b)
