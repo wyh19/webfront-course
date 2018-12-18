87f6f06e81941d15ae29b42178809f3be2bc795a
wyh19 <wyh_19@163.com>
2018-12-18 09:46:03 +0800

    优化

--- xx-测试技术/1-interface-test/index.js
+++ xx-测试技术/1-interface-test/index.js
@@ -12,7 +12,7 @@ var getSkillListUrl = '/manage/v1/skill/list'
 
 axios.get(baseUrl+getSkillListUrl).then(res =>{
      console.log(res.data)
-     console.log('------json对象转字符串------')
+     console.log('------json对象转字符串，便于打印显示------')
      console.log(JSON.stringify(res.data))
  })
 
