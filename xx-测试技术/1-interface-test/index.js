0b0060fb8264e7e532d8d54eef996d6895d689f7
wyh19 <wyh_19@163.com>
2018-12-18 09:29:40 +0800

    开发

---
+++ xx-测试技术/1-interface-test/index.js
@@ -0,0 +1,16 @@
+const axios  = require('axios')
+
+//接口的基本地址
+var baseUrl = 'http://10.20.222.151:9288/skill-platform'
+
+/**
+ * 接口1：获取技能列表
+ * 请求方式：GEt
+ *请求参数：无
+ */
+var getSkillListUrl = '/manage/v1/skill/list'
+
+axios.get(baseUrl+getSkillListUrl).then(res =>{
+     console.log(res)
+ })
+
---
+++ xx-测试技术/1-interface-test/readme.md
@@ -0,0 +1,4 @@
+## 安装
+1. 进入 1-interface-test文件夹内部，右键打开gitbash
+2. gitbash里 输入 npm install  ，等待安装完成
+3. 打开 index.js，按F5调试，查看打印输出
